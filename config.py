544ba7bd99fe07844d8eccf5e752036ef533c4ae
LukasMut <muttenthaler@cbs.mpg.de>
2022-10-24 13:02:37 +0200

    smal change in strength of l2 regularization

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ def get_configs(args, **kwargs):
 
     model_config.regularization = args.regularization
     if args.regularization:
-        model_config.weight_decay = 1e-5
+        model_config.weight_decay = 1e-4
     model_config.n_classes = args.n_classes
     model_config.task = "Odd-one-out"
     # TODO: enable half precision when running things on TPU
