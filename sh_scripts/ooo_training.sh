12fd41ed4f647752d51b8addef721d58c9902125
LukasMut <muttenthaler@cbs.mpg.de>
2022-10-24 11:25:21 +0200

    updated bash script according to most recent code changes

--- sh_scripts/ooo_training.sh
+++ sh_scripts/ooo_training.sh
@@ -6,7 +6,7 @@
 #$ -l cuda=1 -l gputype='A100*'
 #$ -l h_vmem=16G 
 #$ -l mem_free=16G
-#$ -t 1-35
+#$ -t 1-140
 
 # submit as: qsub -m eas -M muttenthaler@cbs.mpg.de ./sh_scripts/ooo_heterogeneous_pretraining.sh
 
@@ -15,22 +15,22 @@ out_path="/home/space/OOOPretraining/results";
 data_path="/home/space/datasets/${dataset}/processed";
 network='Custom';
 
-sampling='dynamic';
 testing='uniform';
 n_classes=10;
+min_samples=5;
 optim='sgd';
 burnin=30;
 patience=10;
 steps=40;
 
-min_samples=( 4 4 5 5 );
-p_masses=( 0.8 0.85 0.9 0.95 ); 
+sampling_strategies=( 'uniform' 'dynamic' );
+probability_masses=( 0.8 0.85 0.9 0.95 ); 
 samples=( 20 30 40 50 100 500 1000 );
 max_epochs=( 300 300 300 300 200 200 200 );
 ooo_batch_sizes=( 32 64 64 64 128 128 128 );
 main_batch_sizes=( 16 16 16 32 32 64 128 );
 etas=( 0.001 0.001 0.001 0.001 0.001 0.001 0.001 );
-max_triplets=( 1000 1000 2000 2000 3000 4000 5000 );
+max_triplets=( 1000 1500 2000 3000 4000 5000 10000 );
 seeds=( 0 1 2 3 4 );
 
 source ~/.bashrc
@@ -44,12 +44,12 @@ export XLA_PYTHON_CLIENT_ALLOCATOR=platform
 logdir="./logs/${dataset}/${network}/${dist}/$SGE_TASK_ID";
 mkdir -p $logdir;
 
-for i in ${!p_masses[@]}; do
+echo "Started odd-one-out learning $SGE_TASK_ID for $network at $(date)"
 
-	echo "Started odd-one-out supervised learning $SGE_TASK_ID for $network at $(date)"
+for sampling in "${sampling_strategies[@]}"; do
 
-	python main.py --out_path $out_path --data_path $data_path --network $network --dataset $dataset --samples ${samples[@]} --optim $optim --sampling $sampling --min_samples ${min_samples[$i]} --probability_mass ${p_masses[$i]} --n_classes $n_classes --max_triplets ${max_triplets[@]} --ooo_batch_sizes ${ooo_batch_sizes[@]} --main_batch_sizes ${main_batch_sizes[@]} --epochs ${max_epochs[@]} --etas ${etas[@]} --burnin $burnin --patience $patience --steps $steps --seeds ${seeds[@]} >> ${logdir}/${task}_${dist}.out
+	python main.py --out_path $out_path --data_path $data_path --network $network --dataset $dataset --samples ${samples[@]} --optim $optim --sampling $sampling --min_samples $min_samples --probability_masses ${probability_masses[@]} --n_classes $n_classes --max_triplets ${max_triplets[@]} --ooo_batch_sizes ${ooo_batch_sizes[@]} --main_batch_sizes ${main_batch_sizes[@]} --epochs ${max_epochs[@]} --etas ${etas[@]} --burnin $burnin --patience $patience --steps $steps --seeds ${seeds[@]} >> ${logdir}/${dataset}_${sampling}.out
 
-	printf "Finished odd-one-out semi-supervised learning $SGE_TASK_ID for $network at $(date)\n"
 done
-# rm -r $cuda_dir;
+
+printf "Finished odd-one-out learning $SGE_TASK_ID for $network at $(date)\n"
