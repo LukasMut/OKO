0bbf6edfed130ab7ff32011efa088289ad95e400
LukasMut <muttenthaler@cbs.mpg.de>
2022-11-04 16:38:41 +0100

    updated bash scripts

--- sh_scripts/ooo_training.sh
+++ sh_scripts/ooo_training.sh
@@ -6,7 +6,7 @@
 #$ -l cuda=1 -l gputype='A100*'
 #$ -l h_vmem=16G 
 #$ -l mem_free=16G
-#$ -t 1-140
+#$ -t 1-120
 
 # submit as: qsub -m eas -M muttenthaler@cbs.mpg.de ./sh_scripts/ooo_heterogeneous_pretraining.sh
 
@@ -19,18 +19,18 @@ testing='uniform';
 n_classes=10;
 min_samples=5;
 optim='sgd';
-burnin=30;
-patience=10;
+burnin=35;
+patience=15;
 steps=40;
 
 sampling_strategies=( 'uniform' 'dynamic' );
 probability_masses=( 0.8 0.85 0.9 0.95 ); 
-samples=( 20 30 40 50 100 500 1000 );
-max_epochs=( 300 300 300 300 200 200 200 );
-ooo_batch_sizes=( 32 64 64 64 128 128 128 );
-main_batch_sizes=( 16 16 16 32 32 64 128 );
-etas=( 0.001 0.001 0.001 0.001 0.001 0.001 0.001 );
-max_triplets=( 1000 1500 2000 3000 4000 5000 10000 );
+samples=( 40 50 100 500 1000 2000 );
+max_epochs=( 300 300 300 200 200 200 );
+ooo_batch_sizes=( 64 64 64 128 128 128 );
+main_batch_sizes=( 16 16 32 32 64 128 );
+etas=( 0.001 0.001 0.001 0.001 0.001 0.001 );
+max_triplets=( 2000 3000 4000 5000 10000 20000 );
 seeds=( 0 1 2 3 4 );
 
 source ~/.bashrc
@@ -48,7 +48,7 @@ for sampling in "${sampling_strategies[@]}"; do
 	logdir="./logs/${dataset}/${network}/${sampling}/$SGE_TASK_ID";
 	mkdir -p $logdir;
 
-	python main.py --out_path $out_path --data_path $data_path --network $network --dataset $dataset --samples ${samples[@]} --optim $optim --sampling $sampling --min_samples $min_samples --probability_masses ${probability_masses[@]} --n_classes $n_classes --max_triplets ${max_triplets[@]} --ooo_batch_sizes ${ooo_batch_sizes[@]} --main_batch_sizes ${main_batch_sizes[@]} --epochs ${max_epochs[@]} --etas ${etas[@]} --burnin $burnin --patience $patience --steps $steps --seeds ${seeds[@]} >> ${logdir}/ooo_${sampling}.out
+	python main.py --out_path $out_path --data_path $data_path --network $network --dataset $dataset --samples ${samples[@]} --optim $optim --sampling $sampling --min_samples $min_samples --probability_masses ${probability_masses[@]} --n_classes $n_classes --max_triplets ${max_triplets[@]} --ooo_batch_sizes ${ooo_batch_sizes[@]} --main_batch_sizes ${main_batch_sizes[@]} --epochs ${max_epochs[@]} --etas ${etas[@]} --burnin $burnin --patience $patience --steps $steps --seeds ${seeds[@]} --regularization >> ${logdir}/ooo_${sampling}.out
 
 done
 
