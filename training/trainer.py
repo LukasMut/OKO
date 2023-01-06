6ef2025367b213909697cd7685732fbd09542bd1
LukasMut <muttenthaler@cbs.mpg.de>
2023-01-06 11:31:40 +0100

    trainer updates on gpu

--- training/trainer.py
+++ training/trainer.py
@@ -98,7 +98,7 @@ class OKOTrainer:
             self.optimizer_config.momentum,
         )
 
-        self.gpu_devices = jax.local_devices(backend="cpu")
+        self.gpu_devices = jax.local_devices(backend="gpu")
 
         # initialize two empty lists to store train and val performances
         self.train_metrics = list()
