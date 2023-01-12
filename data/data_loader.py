fixed problem in normalization

data/data_loader.py
@@ -389,8 +389,8 @@ class OKOLoader:
                     )
                 )
             X = self.X[np.asarray(subset)]
-            if self.data_config.is_rgb_dataset:
-                X = self._normalize(X)
+            # if self.data_config.is_rgb_dataset:
+            #    X = self._normalize(X)
             y = self.y[np.asarray(subset)]
             yield (X, y)
 